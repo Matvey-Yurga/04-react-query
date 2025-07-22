99c14f01bb94a19b4aba200aa9ff1fdc30213d2f
matvey <motaurga1@gmail.com>
2025-07-22 22:01:56 +0300

    add all

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar'
 import css from "./App.module.css"
 import toast, { Toaster } from "react-hot-toast";
@@ -8,42 +8,63 @@ import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([])
-  const [error, setError] = useState(false)
-  const [isLoader, setIsLoader] = useState(false)
-  const [currentMovie,setcurrentMovie] = useState<Movie | null>(null)
-  const handleSubmit = (query: string) => {
-    setMovies([])
-    setIsLoader(true)
-    setError(false)
-    fetchMovies(query).then(data => {
-      if (!data.length) {
-        toast.error("No movies found for your request.")
-      }
-      setMovies(data)
-    }).catch(() => {
-      setError(true)
-    }).finally(() => {
-      setIsLoader(false)
-    });
+const [query, setQuery]=useState("")
+  const [page, setPage] = useState(1)
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  };
-  function onSelect(movie: Movie): void {
-   setcurrentMovie(movie)
+  const {data, isLoading, isError, isSuccess } = useQuery({
+    queryKey:["myQueryKey", query, page],
+    queryFn:  () => fetchMovies(query, page),
+    enabled: query !== "",
+    placeholderData: keepPreviousData
+  })
+  useEffect(() => {
+    if (data?.results.length === 0) {
+       toast.error("No movies found for your request.")
+    }
+    return;
+  },[data, isSuccess])
+
+const totalPages = data?.total_pages ?? 0
+ 
+  const handleSubmit = (newQuery: string) => {
+    setQuery(newQuery)
+    setPage(1)
+    setSelectedMovie(null)
+  }
+ function onSelect(movie: Movie): void {
+   setIsModalOpen(true)
+   setSelectedMovie(movie)
   }
   function closeModal() {
-    setcurrentMovie(null)
+    setSelectedMovie(null)
+    setIsModalOpen(false)
   }
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSubmit} />
-      <Toaster/>
-        {isLoader && <Loader />}
-        {error && <ErrorMessage/>}
-      {movies.length > 0 && <MovieGrid movies={movies} onSelect={onSelect} />}
-      {currentMovie && (
-        <MovieModal movie={currentMovie} onClose={closeModal}/>
+      <Toaster />
+      { isSuccess && data?.results.length > 0 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setPage(selected + 1)}
+          forcePage={page - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />)}
+      {isLoading && <Loader />}
+        {isError && <ErrorMessage/>}
+      {data && data.results.length > 0 && <MovieGrid onSelect={onSelect} movies={data?.results} />}
+      {isModalOpen && selectedMovie &&  (
+        <MovieModal movie={selectedMovie} onClose={closeModal}/>
       )}
       </div>
   )
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -6,14 +6,18 @@ interface MovieResponse {
   page: number;
   total_pages: number;
 }
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (query: string, page: number): Promise<MovieResponse> => {
     const myKey = import.meta.env.VITE_API_KEY;
-    const response = await axios.get<MovieResponse>(`https://api.themoviedb.org/3/search/movie?query=${query}`,
+    const response = await axios.get<MovieResponse>(`https://api.themoviedb.org/3/search/movie`,
         {
+            params: {
+                query,  
+                page,
+            },
             headers: {
                 Authorization: `Bearer ${myKey}`
             }
         }
     )
-    return response.data.results
+    return response.data;
 }
